2bdc401ff8ecfa0813b8595bd84afa05a5e74d7d
ILhankhondaker <ilhankhondaker@gmail.com>
2023-12-12 05:32:21 +0600

    content up

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -4,44 +4,44 @@ const Home = () => {
         <div className="">
             <h1 className="text-center text-5xl font-bold">শিগরই দোয়া  সমুহ সংযোগ করা হবে </h1>
 
-            <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 mt-20">
+            <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-5 mt-20">
                 <div className="p-3 bg-base-200 rounded-lg ">
 
 
                     <iframe className="rounded mx-auto w-11/12 h-52 mx-auto w-11/12 h-52 mx-auto w-11/12 h-52" src="https://www.youtube.com/embed/oN428nn6dG0" title="প্রতিদিন আমলের জন্য সকালের যিক্‌র ┇ Adhkar as Sabah recited by Omar Hisham Al Arabi ┇ أذكار الصباح" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                    <h1>প্রতিদিন আমলের জন্য সকালের যিক্‌র (২৮.২২ মিনিট) </h1>
+                    <h1 className="font-bold">প্রতিদিন আমলের জন্য সকালের যিক্‌র (২৮.২২ মিনিট) </h1>
                 </div>
                 <div className="p-3 bg-base-200 rounded-lg">
 
                     <iframe className="rounded mx-auto w-11/12 h-52 mx-auto w-11/12 h-52" src="https://www.youtube.com/embed/AVIN23l8G1s" title="প্রতিদিন আমলের জন্য সকালের যিক্‌র | Adhkar al Sabah | أذكار الصباح | Quraniyat | An Nafee" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                    <h1>প্রতিদিন আমলের জন্য সকালের যিক্‌র (১৮ মিনিট)</h1>
+                    <h1 className="font-bold">প্রতিদিন আমলের জন্য সকালের যিক্‌র (১৮ মিনিট)</h1>
                 </div>
                 <div className="p-3 bg-base-200 rounded-lg">
 
                     <iframe className="rounded mx-auto w-11/12 h-52 mx-auto w-11/12 h-52" src="https://www.youtube.com/embed/N5ycTmWRavQ" title="প্রতিদিন আমলের জন্য রাতের যিক্‌র | Evening Adhkar | Recited by Omar Hisham Al Arabi | اذكار المساء" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                    <h1>প্রতিদিন আমলের জন্য রাতের যিক্‌র </h1>
+                    <h1 className="font-bold">প্রতিদিন আমলের জন্য রাতের যিক্‌র </h1>
                 </div>
                 <div className="p-3 bg-base-200 rounded-lg">
 
                     <iframe className="rounded mx-auto w-11/12 h-52 mx-auto w-11/12 h-52" src="https://www.youtube.com/embed/KGUUWufvu6E" title="সূরা মুলকের স্বর্গীয়  কোরআন  তেলাওয়াত  ।  Recited  By Omar Hisham Al Arabi | Sikhun Sura" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                    <h1>প্রতিদিন ঘুমানোর আগে</h1>
+                    <h1 className="font-bold">প্রতিদিন ঘুমানোর আগে</h1>
                 </div>
                 <div className="p-3 bg-base-200 rounded-lg">
 
                     <iframe className="rounded mx-auto w-11/12 h-52 mx-auto w-11/12 h-52" src="https://www.youtube.com/embed/1Awjg2Zq6TU" title="আয়াতুল কুরসি ❤️ বাকারার শেষ তিন আয়াত ❤️ হাশরের শেষ তিন আয়াত ❤️ ayatul kursi sura baqarah sura hasor" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                    <h1>আয়াতুল কুরসি, বাকারার শেষ তিন আয়াত, হাশরের শেষ তিন আয়াত </h1>
+                    <h1 className="font-bold">আয়াতুল কুরসি, বাকারার শেষ তিন আয়াত, হাশরের শেষ তিন আয়াত </h1>
+                </div>
+                <div className="p-3 bg-base-200 rounded-lg">
+
+
+                    <iframe className="rounded mx-auto w-11/12 h-52 mx-auto w-11/12 h-52" src="https://www.youtube.com/embed/5R06LRdUCSE" title="আবেগময় সূরা কাহফ এর তিলাওয়াত ┇ Beautiful Surah Kahf Recited by Ismail Al Nouri ┇ An Nafee ┇ আন নাফী" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    <h1 className="font-bold">প্রতিশুক্রবার  সুরা কাহফ  </h1>
                 </div>
                 <div className="p-3 bg-base-200 rounded-lg">
 
 
                     <iframe className="" src="https://www.youtube.com/embed/EZquWudivQQ" title="abdur Rahman mossad Quran recitation episode 81…" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                    <h1>test </h1>
-                </div>
-                <div className="p-3 bg-base-200 rounded-lg">
-
-
-                    <iframe className="" src="https://www.youtube.com/embed/EZquWudivQQ" title="abdur Rahman mossad Quran recitation episode 81…" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                    <h1>test </h1>
+                    <h1 className="font-bold">test </h1>
                 </div>
             </div>
 
