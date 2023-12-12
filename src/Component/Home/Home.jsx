6be512f566321e1f7b47fe5124e89159ad5bdd54
ILhankhondaker <ilhankhondaker@gmail.com>
2023-12-12 06:52:25 +0600

    add data

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,7 +1,7 @@
 
 const Home = () => {
     return (
-        <div className="">
+        <div className=" my-20">
             <h1 className="text-center text-5xl font-bold">শিগরই দোয়া  সমুহ সংযোগ করা হবে </h1>
 
             <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-5 mt-20">
@@ -24,7 +24,17 @@ const Home = () => {
                 <div className="p-3 bg-base-200 rounded-lg">
 
                     <iframe className="rounded mx-auto w-11/12 h-52 mx-auto w-11/12 h-52" src="https://www.youtube.com/embed/KGUUWufvu6E" title="সূরা মুলকের স্বর্গীয়  কোরআন  তেলাওয়াত  ।  Recited  By Omar Hisham Al Arabi | Sikhun Sura" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                    <h1 className="font-bold">প্রতিদিন ঘুমানোর আগে</h1>
+                    <h1 className="font-bold">প্রতিদিন ঘুমানোর আগে সূরা মুলক </h1>
+                </div>
+                <div className="p-3 bg-base-200 rounded-lg">
+
+                    <iframe className="rounded mx-auto w-11/12 h-52 mx-auto w-11/12 h-52" src="https://www.youtube.com/embed/Ufaht91ktps" title="তওবার সর্বশ্রেষ্ঠ দুআ - সাইয়েদুল ইস্তেগফার ┇ Sayyidul Istighfar recited by Sheikh Masud" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    <h1 className="font-bold">সাইয়েদুল ইস্তেগফারভঁ </h1>
+                </div>
+                <div className="p-3 bg-base-200 rounded-lg">
+
+                    <iframe className="rounded mx-auto w-11/12 h-52 mx-auto w-11/12 h-52" src="https://www.youtube.com/embed/uWGTFeB_kSI" title="আল্লাহর ৯৯ নাম সমূহের ফজিলত বাংলা অর্থ ও ফজিলত | 99 Names of Allah | আল্লাহর ৯৯ নাম" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    <h1 className="font-bold">আল্লাহর ৯৯ নাম সমূহের ফজিলত বাংলা অর্থ ও ফজিলত </h1>
                 </div>
                 <div className="p-3 bg-base-200 rounded-lg">
 
@@ -40,8 +50,20 @@ const Home = () => {
                 <div className="p-3 bg-base-200 rounded-lg">
 
 
-                    <iframe className="" src="https://www.youtube.com/embed/EZquWudivQQ" title="abdur Rahman mossad Quran recitation episode 81…" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                    <h1 className="font-bold">test </h1>
+                    <iframe className="rounded mx-auto w-11/12 h-52 mx-auto w-11/12 h-52" src="https://www.youtube.com/embed/OwwIT4y4__4" title="Full Quran 1- 30 Para ,সু মধুর কন্ঠে সম্পূর্ণ ৩০পারা কোরআন তেলাওয়াত। al quran, quran explorer" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    <h1 className="font-bold">সম্পূর্ণ ৩০পারা কোরআন তেলাওয়াত।  </h1>
+                </div>
+                <div className="p-3 bg-base-200 rounded-lg">
+
+
+                    <iframe className="rounded mx-auto w-11/12 h-52 mx-auto w-11/12 h-52" src="https://www.youtube.com/embed/jmMSKmlZeH0" title="Full Al Quran - Sheikh Mishary Rashid Alafasy [JUZ 1 - JUZ 30]" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    <h1 className="font-bold">কোরান তেলাওয়াত </h1>
+                </div>
+                <div className="p-3 bg-base-200 rounded-lg">
+
+
+                    <iframe className="rounded mx-auto w-11/12 h-52 mx-auto w-11/12 h-52" src="https://www.youtube.com/embed/QV3rAwvvVTs" title="Surah Al Baqarah Full (سورة البقره) HEART TOUCHING RECITATION | Zikrullah TV" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    <h1 className="font-bold">সুরাহ বাকার  </h1>
                 </div>
             </div>
 
